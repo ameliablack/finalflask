0c4741784bd0d2d1653b145d9dbc9d0fefc3ab76
ameliablack <72445369+ameliablack@users.noreply.github.com>
2021-05-02 23:57:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from faker import Faker
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField, TextField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user
 
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'asdfasdfasdfasdf'
 
 db = SQLAlchemy(app)
-fake = Faker()
+
 #Creates the database
 
 login_manager = LoginManager(app)
@@ -44,7 +44,8 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(120))
     first_name = db.Column(db.String(120))
     email = db.Column(db.String(120))
-    offer = db.relationship('item', secondary=offer, backref=db.backref('bids', lazy='dynamic'), lazy='dynamic')
+    offers = db.relationship('Item', secondary=offer, backref=db.backref('bids', lazy='dynamic'), lazy='dynamic')
+    
     
     @property
     def password(self):
@@ -64,25 +65,39 @@ class User(UserMixin, db.Model):
 class Item(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
-    item_name = db.Column(db.String(164), nullable=False)
+    itemname = db.Column(db.String(64), nullable=False)
     item_price = db.Column(db.Integer, nullable=False )
-    min_price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(164), nullable=False)
-    bids = db.relationship('user', secondary=offer, backref=db.backref('offer', lazy='dynamic'))
-
+    
+    def __init__(self, itemname, item_price, description):
+       self.itemname = itemname
+       self.item_price = item_price
+       self.description = description
+      
     def __repr__(self):
-        return '<Item {}>'.format(self.item_name)
+        return '<Item {}>'.format(self.itemname)
 
 #OFFERS
+class Offers(db.Model):
+
+    __tablename__ = 'Offers'
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('Article.id'), nullable=False)
+    user_id = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Offer {}>'.format(id)
 
 
-
- #login in 
+ 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Submit')
-#set up web forms for registery
+
+
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1,64)])
     username = StringField('Username', validators=[DataRequired(), Length(1,64)])
@@ -97,10 +112,12 @@ class RegisterForm(FlaskForm):
             raise ValidationError('Username already in use') 
 
 class ItemForm(FlaskForm):
-    item_name = StringField('Name', validators=[DataRequired()])
-    item_price = IntegerField('Bid')
-    description = TextAreaField('Description')
+    itemname = TextField('Item Name', validators=[DataRequired(), Length(1,64)])
+    item_price = IntegerField('Item Price', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
 
 @login_manager.user_loader 
 def load_user(id):
@@ -114,8 +131,10 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm() 
+    print("before validation")
     if form.validate_on_submit(): 
-        user = User(username=form.username.data, password=form.password.data) 
+        print("after validation")
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data) 
         db.session.add(user) 
         db.session.commit() 
         flash('You can now login.')
@@ -127,25 +146,24 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first() 
-        if user is not None and user.verify_password(form.password.data): 
-            login_user(user) 
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
             flash("Logged in.")
             return redirect(url_for('index'))
         flash('Incorrect Information')
         return redirect(url_for('login'))
     return render_template('login.html', form=form)
 
+
 @app.route('/itempage', methods=['GET', 'POST'])
 def itempage():
     form = ItemForm() 
     if form.validate_on_submit():
-        item = Item(item_name=form.item_name.data, item_price=form.item_price.data) 
+        item = Item(itemname=form.itemname.data, item_price=form.item_price.data, description=form.description.data)
         db.session.add(item) 
         db.session.commit() 
-        flash('You can now login.')
         return redirect(url_for('itempage'))
     return render_template('itempage.html', form=form)
-
 #logout
 @app.route('/logout')
 def logout():
@@ -179,19 +197,19 @@ def bidder(username):
 #Item Listing - displays a list of all items
 @app.route('/items')
 def all_items():
-    item_data = Item.query.all(item_id)
+    item_data = Item.query.all()
     return render_template('items.html', item_data=item_data)
 
 @app.route('/items/<item_id>')
-def item(item_id):
-    item_data = Item.query.get_or_404(int(item_id))
-    return render_template('items.html', item_data=item_data)
+def item(itemname):
+    item_data = Item.query.get_or_404(int(id))
+    return render_template('items.html', item_data=item_item_data)
 
 
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', first_name=current_user.first_name)
+    return render_template('profile.html', username=current_user.username)
 
 
 #Item page - displays an item and all of the current bids on it.  If the auction is on-going, logged-in Bidders can add a bid.  If the auction is over, no more bids are allowed
@@ -219,3 +237,4 @@ def profile():
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
